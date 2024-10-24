7edce8b0b4e3b857730a1472a03b22b988d690fb
贾皓元 <jiahaoyuan@MacBook-Air-3.local>
2024-10-24 13:30:20 +0100

    Added Tommy function

--- first_repo/my_function.py
+++ first_repo/my_function.py
@@ -2,3 +2,5 @@ def my_function():
  print("Hello from my_function!")
 def michael_function()
  print("hi this is Michael, hope this time it works")
+def tommy_fun():
+ print("this is Tommy speaking")
